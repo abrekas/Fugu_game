69d9fed8eab74fbb4043a19d829812f9756d90f6
abrekas <alexeizhirnov06@gmail.com>
2025-04-01 19:01:22 +0500

    implemented correct movement but too fast on horizontal (WIP)

---
+++ .moveVMfiles.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem Укажите исходную и целевую папки (измените пути на свои)
+set "source_folder=.\src"
+set "dest_folder=.\vm"
+
+rem Создать целевую папку, если её нет
+if not exist "%dest_folder%" mkdir "%dest_folder%"
+
+rem Переместить все файлы .vm
+move /Y "%source_folder%\*.vm" "%dest_folder%"
+
+rem Сообщение о завершении
+echo Все файлы .vm перемещены в %dest_folder%
+pause
